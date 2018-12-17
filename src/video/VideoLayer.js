bd2d4a3e09ca626bf13161ac8d8c663cc363c0bd
shrekshrek <shrekshrek@gmail.com>
2018-12-17 17:13:38 +0800

    update

--- src/video/VideoLayer.js
+++ src/video/VideoLayer.js
@@ -35,31 +35,71 @@ VideoLayer.prototype = Object.assign(Object.create(Layer.prototype), {
     },
 
     init: function () {
-        // this.globalCanvas = document.createElement('canvas');
-
-        this.vplayer = new Vplayer('./media/v1', {
-            width: this.originRect.width,
-            height: this.originRect.height,
-            // canvas: this.globalCanvas,
-            onStart: () => {
-                this.dispatchEvent('start');
-            },
-            onEnd: () => {
-                this.dispatchEvent('end');
-            }
-        });
-        this.el.appendChild(this.vplayer.el);
-
-        this.resize();
+        model.globalCanvas = document.createElement('canvas');
+        this.curVideoId = null;
+        this.isMuted = false;
+        this.videos = {};
     },
 
     resize: function () {
         Layer.prototype.resize.call(this);
 
-        if (this.vplayer) {
-            this.vplayer.resize(this.originRect.width * this.modifyRect.scale, this.originRect.height * this.modifyRect.scale);
-            this.vplayer.el.style.top = (window.innerHeight - this.originRect.height * this.modifyRect.scale) / 2 + 'px';
+        for (var i in this.videos) {
+            var _player = this.videos[i];
+            if (_player) {
+                _player.resize(this.originRect.width * this.modifyRect.scale, this.originRect.height * this.modifyRect.scale);
+                _player.el.style.top = (window.innerHeight - this.originRect.height * this.modifyRect.scale) / 2 + 'px';
+            }
         }
+    },
+
+    createPlayer: function (name) {
+        var _player = new Vplayer('./media/' + name, {
+            width: this.originRect.width * this.modifyRect.scale,
+            height: this.originRect.height * this.modifyRect.scale,
+            canvas: model.globalCanvas,
+            onStart: () => {
+                this.dispatchEvent('start', name);
+            },
+            onEnd: () => {
+                this.dispatchEvent('end', name);
+            }
+        });
+        _player.el.style.top = (window.innerHeight - this.originRect.height * this.modifyRect.scale) / 2 + 'px';
+        _player.muted(this.isMuted);
+        this.videos[name] = _player;
+    },
+
+    muted: function (bool) {
+        for (var i in this.videos) {
+            var _player = this.videos[i];
+            _player.muted(bool);
+        }
+        this.isMuted = bool;
+    },
+
+    play: function (id, time) {
+        if (this.videos[id] == undefined) this.createPlayer(id);
+        if (this.curVideoId != null && this.curVideoId != id) this.pause();
+        this.curVideoId = id;
+        var _player = this.videos[this.curVideoId];
+        this.el.appendChild(_player.el);
+        _player.play(time);
+    },
+
+    pause: function () {
+        if (this.curVideoId == null) return;
+        var _player = this.videos[this.curVideoId];
+        _player.pause();
+        this.el.removeChild(_player.el);
+        this.curVideoId = null;
+    },
+
+    ready: function (id) {
+        if (this.videos[id] == undefined) this.createPlayer(id);
+        var _player = this.videos[id];
+        _player.play();
+        _player.pause();
     }
 
 });
